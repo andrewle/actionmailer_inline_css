632c741acc0b58e3ebb7790d253c18164627051c
Andrew Le <andrew@andrewdle.com>
2015-07-01 13:28:08 -0700

    Support asset pipeline for loading CSS files
    When running in Rails development mode using the asset pipeline and on a
    single threaded server, CSS files were not getting properly loaded from
    the rendered HTML. Further inspection determined that the `href`
    attributes were from `link` tags were getting stripped by the
    `premailer` gem (see `Premailer#convert_inline_links`,
    https://github.com/premailer/premailer/blob/master/lib/premailer/premailer.rb#L412).
    This is not an issue when in production as absolute URIs are given for
    the assets by default, causing `Premailer` to load from the uri rather
    than try to open the relative path locally.
    
    The result is that an empty `link_uri` is used in
    `Premailer#load_css_from_html!` when `link` tags are searched and
    iterated over, causing load failures in development. The first work
    around that needs to be added to a Rails project is to update your
    environment file so that absolute URIs are generated by the asset
    pipeline, allowing `Premailer` to pass on these hrefs when converting
    inline links. I recommend that you update both your development and
    production configuration as below:
    
    ```
    config.action_mailer.asset_host = "http://your-domain.com"
    ```
    
    Next, this patch overrides the `Premailer#load_css_from_html!` so that,
    in development, we do the following:
    
    1. Clean up the `link`'s href to remove the protocol, domain, asset
       prefix, and fingerprint.
    2. Use Sprockets' `find_asset` to find the given asset.
    3. Load the asset directly as a string (Sprockets has already any/all of
       the necessary preprocessing).
    
    That that fix in place, I was able to get ActionMailer previews set up
    with Premailer inlining CSS. The fix is a monkey-patched approach, which
    is not ideal, and can use some refactoring. Unfortunately, `Premailer`
    does not provide any seams where a cleaner approach could have been
    taken. And, of course, knowledge of Rails' needs should be kept out of
    `Premailer`.
    
    I think that a cleaner upstream workaround could be to add an ability to
    ignore certain URIs in `Premailer#convert_inline_links`.

--- lib/overrides/premailer/premailer.rb
+++ lib/overrides/premailer/premailer.rb
@@ -19,5 +19,49 @@ Premailer.class_eval do
   end
   alias_method_chain :load_css_from_local_file!, :rails_path
 
-end
+  def load_css_from_html!
+    if (@options[:adapter] == :nokogiri)
+      tags = @doc.search("link[@rel='stylesheet']:not([@data-premailer='ignore'])", "//style[not(contains(@data-premailer,'ignore'))]")
+    else
+      tags = @doc.search("link[@rel='stylesheet']:not([@data-premailer='ignore']), style:not([@data-premailer='ignore'])")
+    end
+    if tags
+      tags.each do |tag|
+        if tag.to_s.strip =~ /^\<link/i && tag.attributes['href'] && media_type_ok?(tag.attributes['media']) && @options[:include_link_tags]
+          # A user might want to <link /> to a local css file that is also mirrored on the site
+          # but the local one is different (e.g. newer) than the live file, premailer will now choose the local file
 
+          if tag.attributes['href'].to_s.include? @base_url.to_s and @html_file.kind_of?(String)
+            if @options[:with_html_string]
+              link_uri = tag.attributes['href'].to_s.sub(@base_url.to_s, '').sub(/\A\/*/, '')
+            else
+              link_uri = File.join(File.dirname(@html_file), tag.attributes['href'].to_s.sub!(@base_url.to_s, ''))
+              # if the file does not exist locally, try to grab the remote reference
+              unless File.exists?(link_uri)
+                link_uri = Premailer.resolve_link(tag.attributes['href'].to_s, @html_file)
+              end
+            end
+          else
+            link_uri = tag.attributes['href'].to_s
+          end
+
+          if Rails.env.development?
+            clean_path = link_uri.sub(/\?.*$/, '').sub(%r(^https?://[^/]*/), '/').sub(/(-.*?\..{3,4}$)/, '')
+            asset = Rails.application.assets.find_asset(clean_path.sub("#{Rails.configuration.assets.prefix}/", ''))
+            load_css_from_string(asset.source)
+          elsif Premailer.local_data?(link_uri)
+            $stderr.puts "Loading css from local file: " + link_uri if @options[:verbose]
+            load_css_from_local_file!(link_uri)
+          else
+            $stderr.puts "Loading css from uri: " + link_uri if @options[:verbose]
+            @css_parser.load_uri!(link_uri, {:only_media_types => [:screen, :handheld]})
+          end
+
+        elsif tag.to_s.strip =~ /^\<style/i && @options[:include_style_tags]
+          @css_parser.add_block!(tag.inner_html, :base_uri => @base_url, :base_dir => @base_dir, :only_media_types => [:screen, :handheld])
+        end
+      end
+      tags.remove unless @options[:preserve_styles]
+    end
+  end
+end
